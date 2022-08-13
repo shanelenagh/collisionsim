b984fcfd199c201be63a2d1d33f2f23d43db18c6
Shane Lenagh <shanelenagh@gmail.com>
2022-08-13 16:05:18 -0500

    DRY'ed it up a bit more with X and Y velocity methods, which identified a bug in the collision calc that was fixed, as well as some frame of ref consistency fixes

--- CollisionSimulator.java
+++ CollisionSimulator.java
@@ -38,6 +38,9 @@ public class CollisionSimulator {
 			this.velo = velo;
 			this.heading = heading;
 		}
+		
+		public double veloX() { return Math.cos(Math.toRadians(heading))*velo; }
+		public double veloY() { return Math.sin(Math.toRadians(heading))*velo; }
 	}
 
 	private static class CollisionSimulatorGUI extends Canvas implements Runnable {
@@ -45,9 +48,9 @@ public class CollisionSimulator {
 		private double timeAccelerator = 30.0;
 		private double milesPerPixel = 1.0;
 		private Aircraft ac1 = new Aircraft(new Point2D.Double(0 / milesPerPixel,0 / milesPerPixel),
-			new Color(0, 153, 0), 500.0, 45);
+			new Color(0, 153, 0), 350.0, 60);
 		private Aircraft ac2 = new Aircraft(new Point2D.Double(60 / milesPerPixel,0 / milesPerPixel),
-			new Color(153, 0, 0), 300.0, 0);		
+			new Color(153, 0, 0), 300.0, 90);		
 		private int collisionMilesLimit = 10;
 		private boolean isColliding = false;
 		private double distPx;
@@ -72,10 +75,10 @@ public class CollisionSimulator {
 				if (System.currentTimeMillis() - timeCounter > 1000 || secondCounter == 0) {
 					this.timeCounter = System.currentTimeMillis();
 					this.secondCounter += 1;
-					ac1.loc.x += Math.sin(Math.toRadians(ac1.heading)) * ac1.velo / 60.0 / 60.0 * timeAccelerator;
-					ac1.loc.y += Math.cos(Math.toRadians(ac1.heading)) * ac1.velo / 60.0 / 60.0 * timeAccelerator;
-					ac2.loc.x += Math.sin(Math.toRadians(ac2.heading)) * ac2.velo / 60.0 / 60.0 * timeAccelerator;
-					ac2.loc.y += Math.cos(Math.toRadians(ac2.heading)) * ac2.velo / 60.0 / 60.0 * timeAccelerator;
+					ac1.loc.x += ac1.veloX() / 60.0 / 60.0 * timeAccelerator;
+					ac1.loc.y += ac1.veloY() / 60.0 / 60.0 * timeAccelerator;
+					ac2.loc.x += ac2.veloX() / 60.0 / 60.0 * timeAccelerator;
+					ac2.loc.y += ac2.veloY() / 60.0 / 60.0 * timeAccelerator;
 					this.distPx = Point2D.distance(ac1.loc.x, ac1.loc.y, ac2.loc.x, ac2.loc.y);
 					this.isColliding = distPx*milesPerPixel < collisionMilesLimit;
 					this.isStateChanged = true;
@@ -131,26 +134,26 @@ public class CollisionSimulator {
 				this.ac1Image = this.createImage(10,10);
 				final Graphics2D ac1g = (Graphics2D) this.ac1Image.getGraphics();
 				ac1g.setColor(ac1.color);
-				ac1g.rotate(Math.toRadians(-ac1.heading), 5, 5);
-				ac1g.fillPolygon(new int[] { 0, 10, 5, 0 },  new int[] { 0, 0, 10, 0 }, 4);	
+				ac1g.rotate(Math.toRadians(ac1.heading), 5, 5);
+				ac1g.fillPolygon(new int[] { 0, 0, 10, 0 },  new int[] { 0, 10, 5, 0 }, 4);	
 				ac1g.dispose();
 			}
 			g.drawImage(ac1Image, (int) ac1.loc.x, (int) ac1.loc.y, this);
 			g.setColor(ac1.color);
-			g.drawLine(((int)ac1.loc.x)+5, ((int)ac1.loc.y)+5, ((int)ac1.loc.x)+5+((int)(Math.sin(Math.toRadians(ac1.heading)) * ac1.velo)) / 10, 
-				(int)ac1.loc.y+5+((int)(Math.cos(Math.toRadians(ac1.heading)) * ac1.velo)) / 10);				
+			g.drawLine(((int)ac1.loc.x)+5, ((int)ac1.loc.y)+5, ((int)ac1.loc.x)+5+((int)ac1.veloX()) / 10, 
+				(int)ac1.loc.y+5+((int)ac1.veloY()) / 10);				
 			if (this.ac2Image == null) {
 				this.ac2Image = this.createImage(10,10);
 				final Graphics2D ac2g = (Graphics2D) this.ac2Image.getGraphics();	
 				ac2g.setColor(ac2.color);
-				ac2g.rotate(Math.toRadians(-ac2.heading), 5, 5);
-				ac2g.fillPolygon(new int[] { 0, 10, 5, 0 },  new int[] { 0, 0, 10, 0 }, 4);		
+				ac2g.rotate(Math.toRadians(ac2.heading), 5, 5);
+				ac2g.fillPolygon(new int[] { 0, 0, 10, 0 },  new int[] { 0, 10, 5, 0 }, 4);		
 				ac2g.dispose();
 			}
 			g.drawImage(ac2Image, (int) ac2.loc.x, (int) ac2.loc.y, this);
 			g.setColor(ac2.color);
-			g.drawLine(((int)ac2.loc.x)+5, ((int)ac2.loc.y)+5, ((int)ac2.loc.x)+5+((int)(Math.sin(Math.toRadians(ac2.heading)) * ac2.velo)) / 10, 
-				(int)ac2.loc.y+5+((int)(Math.cos(Math.toRadians(ac2.heading)) * ac2.velo)) / 10);
+			g.drawLine(((int)ac2.loc.x)+5, ((int)ac2.loc.y)+5, ((int)ac2.loc.x)+5+((int)ac2.veloX()) / 10, 
+				(int)ac2.loc.y+5+((int)ac2.veloY()) / 10);
 		}
 			
 		private void drawGridlines(Graphics g) {
@@ -168,18 +171,19 @@ public class CollisionSimulator {
 			g.setColor(Color.LIGHT_GRAY);
 			g.fillRect(0, getHeight()-15, getWidth(), getHeight());
 			g.setColor(Color.BLACK);
-			final String msg = String.format("%ds (%.0fs sim) - green: %.1f,%.1f \u2192 red: %.1f,%.1f = %.1fpx (%.1fnmi) %s",
-				secondCounter, secondCounter*timeAccelerator, ac1.loc.x, ac1.loc.y, ac2.loc.x, ac2.loc.y, distPx, distPx*milesPerPixel, 
-				isColliding ? "COLLIDING" : "");
+			final String msg = String.format(
+				"%ds (%.0fs sim) - green: %.1f,%.1f \u2192 red: %.1f,%.1f = %.1fpx (%.1fnmi) %s",
+				secondCounter, secondCounter*timeAccelerator, ac1.loc.x, ac1.loc.y, ac2.loc.x, ac2.loc.y, 
+				distPx, distPx*milesPerPixel, isColliding ? "COLLIDING" : "");
 			g.drawString(msg, 0, getHeight()-2);
 			System.out.println(msg);
 		}
 		
 		private double calculatedCollisionTime(Aircraft ac1, Aircraft ac2) {
 			final double a = (ac2.loc.x - ac1.loc.x) * milesPerPixel;
-			final double b = ac2.velo*Math.sin(Math.toRadians(-ac2.heading)) - ac1.velo*Math.cos(Math.toRadians(-ac1.heading));
+			final double b = ac2.veloX() - ac1.veloX();
 			final double c = (ac2.loc.y - ac1.loc.y) * milesPerPixel;
-			final double d = ac2.velo*Math.cos(Math.toRadians(-ac2.heading)) - ac1.velo*Math.cos(Math.toRadians(-ac1.heading));
+			final double d = ac2.veloY() - ac1.veloY();
 			
 			return - ((a*b + c*d) / (b*b + d*d));
 		}
